a614dce3a39954edb76d253f093e8f2b0b283c41
painghtetaung <painghtetaung1999@gmail.com>
2022-10-29 20:40:34 +0630

    added mutation addClient

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,7 +6,7 @@
 const Project = require('../models/Project');
 const Client = require('../models/Client');
 
-const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList} = require('graphql');
+const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList, GraphQLNonNull} = require('graphql');
 
 // Project Type
 const ProjectType = new GraphQLObjectType({
@@ -71,6 +71,32 @@ const RootQuery = new GraphQLObjectType({
     }
 });
 
+// Mutations
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        //Add client
+        addClient: {
+            type: ClientType,
+            args: {
+                name: {type: GraphQLNonNull(GraphQLString)},
+                email: {type: GraphQLNonNull(GraphQLString)},
+                phone: {type: GraphQLNonNull(GraphQLString)},
+            },
+            resolve(parent, args) {
+                const client = new Client({
+                    name: args.name,
+                    email: args.email,
+                    phone: args.phone
+                });
+
+                return client.save();
+            }
+        }
+    }
+});
+
 module.exports = new GraphQLSchema({
     query: RootQuery,
-})
+    mutation
+});
